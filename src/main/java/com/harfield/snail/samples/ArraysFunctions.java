9a8cce1345770db7d5946df70ca3512e5483a397
harfield <huangrh@fancydsp.com>
2017-08-24 14:23:33 +0800

    add git ignore

--- src/main/java/com/harfield/snail/samples/ArraysFunctions.java
+++ src/main/java/com/harfield/snail/samples/ArraysFunctions.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class ArraysFunctions {
     public static void main(String[] args) {
 //        ArrayList<String> a = new ArrayList<String>("a");
-
+        /*asList 返回的list 没有 add方法*/
         List<String> list = Arrays.asList("a");
         list.add("b");
     }
