1803d28d4452eb8a33ee770b8e4669c4c4a6a50a
harfield <huangrh@fancydsp.com>
2017-08-24 14:17:50 +0800

    add git ignore

---
+++ src/main/java/com/harfield/snail/samples/ArraysFunctions.java
@@ -0,0 +1,16 @@
+package com.harfield.snail.samples;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by harfield on 2017/8/16.
+ */
+public class ArraysFunctions {
+    public static void main(String[] args) {
+//        ArrayList<String> a = new ArrayList<String>("a");
+
+        List<String> list = Arrays.asList("a");
+        list.add("b");
+    }
+}
