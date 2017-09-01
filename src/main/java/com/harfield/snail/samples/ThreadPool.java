a3137982329ea0da2c86fd350037b8be886f1419
harfield <huangrh@fancydsp.com>
2017-09-01 10:01:27 +0800

    add git ignore

--- src/main/java/com/harfield/snail/samples/ThreadPool.java
+++ src/main/java/com/harfield/snail/samples/ThreadPool.java
@@ -6,7 +6,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 /**
- * Created by harfield on 2017/8/24.
+ * Exception in thread pool
  */
 public class ThreadPool {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
